22ad9b5ed1deccff6d46394fb0e2f7c8c238c3b6
viktorkhomyn <viktor.khomyn@sombrainc.com>
2021-08-30 14:15:46 +0300

    first commit

---
+++ WebApplication1/Migrations/20210828174850_ini.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApplication1.Migrations
+{
+    public partial class ini : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "nvarchar(250)",
+                table: "Employees",
+                newName: "FullName");
+
+            migrationBuilder.RenameColumn(
+                name: "nvarchar(100)",
+                table: "Employees",
+                newName: "EmployeePosition");
+
+            migrationBuilder.RenameColumn(
+                name: "nvarchar(10)",
+                table: "Employees",
+                newName: "EmployeeCode");
+
+            migrationBuilder.AddColumn<string>(
+                name: "OfficeLocation",
+                table: "Employees",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OfficeLocation",
+                table: "Employees");
+
+            migrationBuilder.RenameColumn(
+                name: "FullName",
+                table: "Employees",
+                newName: "nvarchar(250)");
+
+            migrationBuilder.RenameColumn(
+                name: "EmployeePosition",
+                table: "Employees",
+                newName: "nvarchar(100)");
+
+            migrationBuilder.RenameColumn(
+                name: "EmployeeCode",
+                table: "Employees",
+                newName: "nvarchar(10)");
+        }
+    }
+}
---
+++ WebApplication1/Models/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Employee
+    {
+        [Key]
+        [DisplayName("ID")]
+        public int EmployeeId { get; set; }
+        [DisplayName("Name")]
+        [Required]
+        public string FullName { get; set; }
+        [DisplayName("Code")]
+        public string EmployeeCode { get; set; }
+        [DisplayName("Position")]
+        public string EmployeePosition { get; set; }
+        [DisplayName("Office location")]
+        public string OfficeLocation { get; set; }
+    }
+}
